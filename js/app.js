fcc7775942a68e0350cebdec643d3f9173f3b4e0
Blake Hagen <blakehagen@gmail.com>
2015-03-07 11:58:14 -0700

    sat 12pm

--- js/app.js
+++ js/app.js
@@ -1,6 +1,25 @@
-var app = angular.module('fitstantly', []);
+var app = angular.module('fitstantly', [/*'ngRoute'*/]);
+
+app.config(function(/*$routeProvider, $location*/) {
+	// $routeProvider
+	// 	.when('/', {
+	// 		templateUrl: '/',
+	// 		controller: 'HomeCtrl'
+	// 	})
+
+	// 	.when('/userId', {
+	// 		templateUrl: '/js/userInfo/userInfoTemplate.html',
+	// 		controller: 'UserCtrl'
+	// 	})
+
+	// 	.otherwise({
+	// 		redirectTo: '/'
+	// 	})
 
 
+
+
+});
 
 
 
--- js/home/homeCtrl.js
+++ js/home/homeCtrl.js
@@ -3,16 +3,28 @@ var app = angular.module('fitstantly');
 app.controller('HomeCtrl', function($scope) {
 
 	OAuth.initialize('YHZC6eo2wgsgM3mAgtgrxFYe9Lw')
-
 	
 	$scope.authorize = function() {
 		OAuth.popup('fitbit', {cache: true}).done(function(fitbit) {
 			console.log(fitbit)
-	  	//make API calls with `fitbit`
-			}).fail(function(err) {
+
+			fitbit.get('https://api.fitbit.com/1/user/2B86FZ/activities/date/2015-02-25.json').done(function(data) {
+				console.log('hello')
+				  //todo with data
+				}).fail(function(err) {
+					console.log('nope')
+				  //todo with err
+				});
+
+		}).fail(function(err) {
 	  			alert("Authentication failed. Please try again.")
 		})
 	}
+
+
+
+
+
 
 
 
