ebd44551bb938ecd5ff3dce021a439432c7ea82e
Blake Hagen <blakehagen@gmail.com>
2015-03-11 18:51:50 -0600

    weds 650pm

--- js/home/homeCtrl.js
+++ js/home/homeCtrl.js
@@ -7,7 +7,7 @@ app.controller('HomeCtrl', function($scope, homeService, $location) {
 	$scope.authorize = function() {
 		homeService.authenticate().then(function(data){
 			$location.path('/userId');
-			console.log(data);
+			// console.log(data);
 			homeService.setSteps(data.summary.steps);
 			homeService.setActive(data.summary.veryActiveMinutes);
 
@@ -16,34 +16,6 @@ app.controller('HomeCtrl', function($scope, homeService, $location) {
 
 
 		
-
-
-
-
-
-
-
-
-
-	// OAuth.initialize('YHZC6eo2wgsgM3mAgtgrxFYe9Lw')
-	
-	// $scope.authorize = function() {
-	// 	//OAuthService.authenticate().then(function(data){
-	// 		// $location.path('/' + data.userid);
-	// 	// })
-	// 	OAuth.popup('fitbit', {cache: true}).done(function(fitbit) {
-	// 		console.log(fitbit)
-
-	// 		fitbit.get('https://api.fitbit.com/1/user/2B86FZ/activities/date/2015-02-25.json').done(function(data) {
-
-	// 			console.log(data);
-	// 		});
-
-	// 	}).fail(function(err) {
-	//   		alert("Authentication failed. Please try again.")
-	// 	})
-	// }
-
 
 
 
--- js/home/homeService.js
+++ js/home/homeService.js
@@ -4,29 +4,58 @@ app.service('homeService', function($q) {
 
 	var currentDate = '';
 	var dateArr = [];
+	var newDateArr = [];
 	var date = Date();
+	var monthNum = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
 
-	console.log(date);
+	// console.log(date);
 	dateArr = date.split(' ');
-	console.log(dateArr);
+	// console.log(dateArr);
+
+	newDateArr.push(dateArr[3]);
+
+	if (dateArr[1] === "Jan") {
+	  	newDateArr.push(monthNum[0]);
+	} else if (dateArr[1] === "Feb") {
+	  	newDateArr.push(monthNum[1]);
+		} else if (dateArr[1] === "Mar") {
+			newDateArr.push(monthNum[2]);
+			} else if (dateArr[1] === "Apr") {
+				newDateArr.push(monthNum[3]);
+				} else if (dateArr[1] === "May") {
+					newDateArr.push(monthNum[4])
+					} else if (dateArr[1] === "Jun") {
+						newDateArr.push(monthNum[5]);
+						} else if (dateArr[1] === "Jul") {
+							newDateArr.push(monthNum[6]);
+							} else if (dateArr[1] === "Aug") {
+							  	newDateArr.push(monthNum[7]);
+								} else if (dateArr[1] === "Sep") {
+									newDateArr.push(monthNum[8]);
+									} else if (dateArr[1] === "Oct") {
+										newDateArr.push(monthNum[9]);
+										} else if (dateArr[1] === "Nov") {
+											newDateArr.push(monthNum[10])
+											} else {
+												newDateArr.push(monthNum[11]);
+												}
+
+	newDateArr.push(dateArr[2]);
+	// console.log(newDateArr);
+
+	var currentDate = newDateArr[0] + "-" + newDateArr[1] + "-" + newDateArr[2];
+	console.log(currentDate);
 
 	
-
-
-
-
-
-
-
 	
 	this.authenticate = function() {
 		
 		var deferred = $q.defer();
 
 		OAuth.popup('fitbit', {cache: true}).done(function(fitbit) {
-			console.log(fitbit)
+			// console.log(fitbit)
 
-			fitbit.get('https://api.fitbit.com/1/user/-/activities/date/2015-02-25.json').done(function(data) {
+			fitbit.get('https://api.fitbit.com/1/user/-/activities/date/' + currentDate + '.json').done(function(data) {
 				deferred.resolve(data);
 
 
