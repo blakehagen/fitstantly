921bcea02f22eafcabc60c9f1bb7af19111b5431
Blake Hagen <blakehagen@gmail.com>
2015-03-20 19:34:36 -0600

    fri 3-20-15 735pm - styling work

--- js/userInfo/userCtrl.js
+++ js/userInfo/userCtrl.js
@@ -2,6 +2,8 @@ var app = angular.module('fitstantly');
 
 app.controller('UserCtrl', function($scope, homeService) {
 
+	
+
 	$scope.todaySteps = homeService.getSteps();
 	$scope.todayActive = homeService.getActive();
 
