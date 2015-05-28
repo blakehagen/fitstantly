f45255d78593d6ec683faf68080ff6db563f554f
Blake Hagen <blakehagen@gmail.com>
2015-05-27 19:12:15 -0600

    updated chart label and icon overlap

--- js/userInfo/userCtrl.js
+++ js/userInfo/userCtrl.js
@@ -58,7 +58,7 @@ app.controller('UserCtrl', function($scope, homeService, $rootScope) {
 //This is the chart on the User View-------------------
 	
 	$scope.labels;
-	$scope.series = ["Steps", "Average"];
+	$scope.series = [" Steps", " 7-day Average"];
   	$scope.data;
 
   	// console.log("chart steps data: " + $scope.data);
