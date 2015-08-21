9ee3d9dab824ee2abbf46e780e7bc9d2545ea173
Blake Hagen <blakehagen@gmail.com>
2015-08-21 15:29:13 -0600

    chart

--- js/userInfo/userCtrl.js
+++ js/userInfo/userCtrl.js
@@ -33,26 +33,26 @@ app.controller('UserCtrl', function($scope, homeService, $rootScope) {
 	// console.log(chartData);
 
 	$scope.labels = [];
-	$scope.data = [[], []];
+	$scope.data = [[]];
 
 	for (var i = 0; i < chartData.length; i++) {
 		$scope.labels.push(chartData[i].dateTime.slice(6).replace("-", "/"));
 		$scope.data[0].push(parseInt(chartData[i].value));
 	};
 
-	var sum = 0;
-	for (var i = 0; i < chartData.length; i++) {
-		var sum = parseInt(chartData[i].value) + sum;
-	}
+	// var sum = 0;
+	// for (var i = 0; i < chartData.length; i++) {
+	// 	var sum = parseInt(chartData[i].value) + sum;
+	// }
 
-	var avg = sum / 7;
-	avg = Math.round(avg);
-	avgStepsWeek = avg;
-	// avgStepsWeek = numeral(avgStepsWeek).format('0,0');
-	// console.log('Avg steps last 7 days: ', avgStepsWeek);
-	for (var i = 0; i < 7; i++) {
-		$scope.data[1].push(parseInt(avgStepsWeek));
-	};
+	// var avg = sum / 7;
+	// avg = Math.round(avg);
+	// avgStepsWeek = avg;
+	// // avgStepsWeek = numeral(avgStepsWeek).format('0,0');
+	// // console.log('Avg steps last 7 days: ', avgStepsWeek);
+	// for (var i = 0; i < 7; i++) {
+	// 	$scope.data[1].push(parseInt(avgStepsWeek));
+	// };
 
 
 //This is the chart on the User View-------------------
