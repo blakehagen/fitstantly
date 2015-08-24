acf1d0e1ab1a2800238434117b7d937161f99f9c
Blake Hagen <blakehagen@gmail.com>
2015-08-24 15:33:30 -0600

    redirect test

--- js/home/homeService.js
+++ js/home/homeService.js
@@ -109,7 +109,7 @@ app.service('homeService', function($q, $http) {
 		var deferred = $q.defer();
 
 		
-		OAuth.redirect('fitbit');
+		OAuth.redirect('fitbit', 'http://localhost/callback');
 		console.log("hello redirect test 1");
 
 
