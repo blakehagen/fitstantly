509dd1d15dbf181f041a47977e0614274c23694e
Blake Hagen <blakehagen@gmail.com>
2015-08-24 20:10:41 -0600

    redirect

--- js/home/homeService.js
+++ js/home/homeService.js
@@ -114,7 +114,7 @@ app.service('homeService', function($q, $http) {
 
 		OAuth.initialize('YHZC6eo2wgsgM3mAgtgrxFYe9Lw');
 
-		OAuth.callback('fitbit', {cache: true}).done(function(fitbit) {
+		OAuth.callback('fitbit').done(function(fitbit) {
 			// console.log(fitbit)
 
 			promisesArray.push(fitbit.get('https://api.fitbit.com/1/user/-/activities/date/' + currentDate + '.json')) // ---> current date steps
